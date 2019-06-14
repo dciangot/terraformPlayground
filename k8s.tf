9e5566a8d33e615ad8341c23f483ab9f5c30ad08
dciangot <diego.ciangottini@cern.ch>
2019-06-14 12:39:29 +0200

    first commit

---
+++ k8s.tf
@@ -0,0 +1,172 @@
+
+# TODO: use modules
+variable "network_name"{
+  default = "dciangot"
+}
+
+variable "image_id" {
+  default = "d9a41aed-3ebf-42f9-992e-ef0078d3de95"
+}
+
+variable "master_flavor_name" {
+  default = "m1.medium"
+}
+
+variable "slave_flavor_name" {
+  default = "m1.large"
+}
+
+variable "n_slaves" {
+  default = 4
+}
+
+variable "volume_size" {
+  default = 50
+}
+
+# GET keys previously generated
+resource "openstack_compute_keypair_v2" "cluster-keypair" {
+  name = "cluster-keypair"
+  public_key = "${file("key.pub")}"
+}
+
+# TODO: volumes e security groups
+resource "openstack_compute_secgroup_v2" "secgroup_k8s" {
+  name        = "secgroup_k8s"
+  description = "security group for k8s with terraform"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 6443
+    to_port     = 6443
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_instance_v2" "k8s-master" {
+  count = 2
+  name      = "master-${count.index}"
+  image_id  = "${var.image_id}"
+  flavor_name = "${var.master_flavor_name}"
+  key_pair  = "${openstack_compute_keypair_v2.cluster-keypair.name}"
+  security_groups = ["${openstack_compute_secgroup_v2.secgroup_k8s.name}"]
+  network {
+    name = "${var.network_name}"
+    access_network = true
+  }
+
+  #network {
+  #  name = "infn-farm"
+  #  access_network = true
+  #}
+
+  provisioner "local-exec" {
+    command = "echo ${openstack_networking_floatingip_v2.floatingip_master.address} >> hosts.txt"
+  }
+
+
+}
+
+#resource "openstack_blockstorage_volume_v2" "volumes" {
+#  count = "${var.n_slaves}"
+#  name        = "cache_${count.index}"
+#  description = "cache volume"
+#  size        = "${var.volume_size}"
+#}
+
+resource "openstack_compute_instance_v2" "k8s-nodes" {
+  name      = "node-${count.index}"
+  count = "${var.n_slaves}"
+  image_id  = "${var.image_id}"
+  flavor_name = "${var.slave_flavor_name}"
+  key_pair  = "${openstack_compute_keypair_v2.cluster-keypair.name}"
+  security_groups = ["default"]
+
+  network {
+    name = "${var.network_name}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.access_ip_v4} >> hosts.txt"
+    
+  }
+
+}
+
+#resource "openstack_compute_volume_attach_v2" "attachments" {
+#  count       = "${var.n_slaves}"
+#  instance_id = "${openstack_compute_instance_v2.k8s-nodes.*.id[count.index]}"
+#  volume_id   = "${openstack_blockstorage_volume_v2.volumes.*.id[count.index]}"
+#}
+
+resource "openstack_networking_floatingip_v2" "floatingip_master" {
+  count = 2
+  pool = "ext-net"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "floatingip_master" {
+  count = 2
+  floating_ip = "${openstack_networking_floatingip_v2.floatingip_master.*.address}"
+  instance_id = "${openstack_compute_instance_v2.k8s-master.*.id}"
+}
+
+
+resource "null_resource" "cluster" {
+
+  triggers = {
+    floating_ip = "${openstack_compute_floatingip_associate_v2.floatingip_master.id}"
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 90 && scp -o \"StrictHostKeyChecking no\" -i key key ubuntu@${openstack_networking_floatingip_v2.floatingip_master.address}:~/.ssh/id_rsa"
+  }
+
+  provisioner "local-exec" {
+    command = "scp -o \"StrictHostKeyChecking no\" -i key key.pub ubuntu@${openstack_networking_floatingip_v2.floatingip_master.address}:~/.ssh/id_rsa.pub"
+  }
+
+  provisioner "file" {
+    source      = "scripts/install.sh"
+    destination = "/tmp/install.sh"
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      host = "${openstack_networking_floatingip_v2.floatingip_master.address}"
+      private_key = "${file("key")}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/install.sh",
+      "chmod 600 ~/.ssh/id_rsa",
+      "export HOSTS=\"(${openstack_compute_instance_v2.k8s-master.access_ip_v4} ${join(" ", openstack_compute_instance_v2.k8s-nodes[*].access_ip_v4)})\"",
+      "/tmp/install.sh"
+    ]
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      host = "${openstack_networking_floatingip_v2.floatingip_master.address}"
+      private_key = "${file("key")}"
+  }
+  }
+}
+
+output "master_ips" {
+  value = ["${openstack_networking_floatingip_v2.floatingip_master.address}"]
+}
+
+output "node_ips" {
+  value = "${openstack_compute_instance_v2.k8s-nodes[*].access_ip_v4}"
+}
+
+output "dashboard_url" {
+  value = ["https://${openstack_networking_floatingip_v2.floatingip_master.address}:6443/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/login"]
+}
---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt update 
+#&& sudo apt upgrade -y
+
+sudo apt install -y  build-essential python3 python3-pip python3-dev libffi-dev git
+sudo pip3 install --upgrade pip
+
+git clone https://github.com/kubernetes-sigs/kubespray.git
+cd kubespray
+
+sudo pip3 install --upgrade cryptography
+sudo pip3 install -r requirements.txt
+cp -rfp inventory/sample inventory/mycluster
+
+declare -a IPS=$HOSTS
+
+echo $IPS
+CONFIG_FILE=inventory/mycluster/hosts.yml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
+
+ansible-playbook -i inventory/mycluster/hosts.yml --become --become-user=root --extra-vars ansible_user=ubuntu --extra-vars kube_network_plugin=flannel cluster.yml
